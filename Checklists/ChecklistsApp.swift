d8d9ff6970eced55a6b721ca3d96205726335da0
Logan Barnes <logan@rotor.ai>
2023-12-14 20:18:31 -0700

    Initial Commit

---
+++ Checklists/ChecklistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistsApp.swift
+//  Checklists
+//
+//  Created by Logan Barnes on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
