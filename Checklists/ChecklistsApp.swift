a3e171f1b15225af37cdf9f6cf6e833c8a239cd1
Logan Barnes <logan@rotor.ai>
2023-12-16 13:12:06 -0700

    Add an app icon

--- Checklists/ChecklistsApp.swift
+++ Checklists/ChecklistsApp.swift
@@ -10,11 +10,13 @@ import SwiftUI
 @main
 struct ChecklistsApp: App {
     @State private var checklistData = ChecklistData()
+    @State private var modelState = ModelState()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(checklistData)
+                .environment(modelState)
         }
     }
 }
